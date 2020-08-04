5a5a8c17c5994ebdda341478a7e6877101e4198a
viniciusdevti <viniciusdevti@gmail.com>
2020-08-03 22:36:30 -0300

    adicionando global css e component landing

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,23 +1,14 @@
 import React from 'react';
 
+import Landing from './pages/Landing'
+
+import './assets/styles/global.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Landing></Landing>
+
     </div>
   );
 }
